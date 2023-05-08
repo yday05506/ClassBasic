dfaf36ae526172416cc790afb0a92003313c5e78
yday05506 <yangdayeon2@gmail.com>
2023-05-08 12:16:58 +0900

    Class

--- ClassBasic/Program.cs
+++ ClassBasic/Program.cs
@@ -4,6 +4,14 @@ namespace MyApp // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
+
+        private class Math { 
+            public int Abs(int a)
+            {
+                return 0;
+            }
+        }
+
         static void Main(string[] args)
         {
             ThirdClass abc;
@@ -11,6 +19,9 @@ namespace MyApp // Note: actual namespace depends on the project name.
             InnerClass inn;
 
             Console.WriteLine("Hello World!");
+
+            Math math = new Math();
+            Console.WriteLine(Math.Abs(-10));
         }
 
         private class InnerClass
