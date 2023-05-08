46b7cb7ce9b157d27630935d8fabd997d02d84b3
yday05506 <yangdayeon2@gmail.com>
2023-05-08 12:12:29 +0900

    초기 커밋.

---
+++ ClassBasic/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ThirdClass abc;
+            ABCClass abc2;
+            InnerClass inn;
+
+            Console.WriteLine("Hello World!");
+        }
+
+        private class InnerClass
+        {
+        }
+    }
+
+    internal class ABCClass
+    {
+    }
+}
