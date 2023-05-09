6b13957fdccff8adae7dae6a0a367280bc6b4786
yday05506 <yangdayeon2@gmail.com>
2023-05-09 10:57:03 +0900

    c# 스타일로 인스턴스 변수 초기화

--- ClassBasic/Program.cs
+++ ClassBasic/Program.cs
@@ -5,7 +5,8 @@ namespace MyApp // Note: actual namespace depends on the project name.
     internal class Program
     {
 
-        private class Math { 
+        private class Math
+        {
             /* static public int Abs(int a)
             {
                 return 0;
@@ -15,7 +16,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
 
         static void Main(string[] args)
         {
-            ThirdClass abc;
+            /* ThirdClass abc;
             ABCClass abc2;
             InnerClass inn;
 
@@ -32,5 +33,20 @@ namespace MyApp // Note: actual namespace depends on the project name.
 
     internal class ABCClass
     {
+    } */
+
+            Product product = new Product();
+
+            // 자바 스타일
+            Product product1 = new Product();
+            product1.name = "감자";
+            product1.price = 2000;
+
+            // C# 스타일
+            Product product2 = new Product() { name = "고구마", price = 2100 };
+            Product product3 = new Product() { name = "옥수수" };
+            Product product4 = new Product() { price = 999999 };
+            Product product5 = new Product() { price = 7777, name = "비트" };
+        }
     }
 }
