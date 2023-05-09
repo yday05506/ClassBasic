7fc06e9ec5ea41071e5ddc1cc8299f6c604cfd02
yday05506 <yangdayeon2@gmail.com>
2023-05-09 12:06:51 +0900

    list에 들어있는 인스턴스를 삭제할 때 주의점

--- ClassBasic/Program.cs
+++ ClassBasic/Program.cs
@@ -57,6 +57,9 @@ namespace MyApp // Note: actual namespace depends on the project name.
             list.Add(new Student() { name = "곽경희", grade = 3 });
             list.Add(new Student() { name = "권하은", grade = 2 });
             list.Add(new Student() { name = "김선혜", grade = 1 });
+            list.Add(new Student() { name = "김하늘", grade = 4 });
+            list.Add(new Student() { name = "배서연", grade = 1 });
+            list.Add(new Student() { name = "백지민", grade = 2 });
 
             List<Student> list2 = new List<Student>() {
                 new Student() { name = "곽경희", grade = 3 },
@@ -70,6 +73,30 @@ namespace MyApp // Note: actual namespace depends on the project name.
             }
 
             foreach (var item in list2)
+            {
+                Console.WriteLine(item);
+            }
+
+            // list 안의 인스턴스 지우기 (예외 발생)
+            /* foreach (var item in list)
+            {
+                if(item.grade == 3)
+                {
+                    list.Remove(item);
+                }
+            } */
+
+            Console.WriteLine("-------------------");
+            // 뒤에서부터 삭제하면 문제를 피할 수 있음
+            for(int i = 0; i < list.Count;  i++)
+            {
+                if (list[i].grade > 1)
+                {
+                    list.RemoveAt(i);
+                }
+            }
+
+            foreach (var item in list)
             {
                 Console.WriteLine(item);
             }
